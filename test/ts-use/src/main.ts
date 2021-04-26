6513572f12fb63d100b5afc4612935608153f380
Rod Vagg <rod@vagg.org>
2021-04-26 14:30:20 +1000

    feat!: remove default export, multiformats@7, full type check & gen (#18)
    BREAKING CHANGE:
    
    * remove `default` export, see below
    * check, generate and publish full TypeScript types
    * use multiformats@7 and its non-default exports and updated types
    * export types based on latest multiformats structure
    * publish with "main"
    
    Prior to this change this module was imported as
    
    ```js
    import dagcbor from '@ipld/dag-cbor'
    ```
    
    Now it is imported as
    
    ```js
    import * as dagcbor from `@ipld/dag-cbor'
    ```

---
+++ test/ts-use/src/main.ts
@@ -0,0 +1,45 @@
+import { deepStrictEqual } from 'assert'
+
+import { BlockEncoder, BlockDecoder, BlockCodec } from 'multiformats/codecs/interface'
+import * as dagCbor from '@ipld/dag-cbor'
+
+const main = () => {
+  // make sure we have a full CodecFeature
+  useCodecFeature(dagCbor)
+}
+
+function useCodecFeature (codec: BlockCodec<0x71, any>) {
+  // use only as a BlockEncoder
+  useEncoder(codec)
+
+  // use only as a BlockDecoder
+  useDecoder(codec)
+
+  // use as a full BlockCodec which does both BlockEncoder & BlockDecoder
+  useBlockCodec(codec)
+}
+
+function useEncoder<Codec extends number> (encoder: BlockEncoder<Codec, string>) {
+  deepStrictEqual(encoder.code, 0x71)
+  deepStrictEqual(encoder.name, 'dag-cbor')
+  deepStrictEqual(Array.from(encoder.encode('blip')), [100, 98, 108, 105, 112])
+  console.log('[TS] ✓ { encoder: BlockEncoder }')
+}
+
+function useDecoder<Codec extends number> (decoder: BlockDecoder<Codec, Uint8Array>) {
+  deepStrictEqual(decoder.code, 0x71)
+  deepStrictEqual(decoder.decode(Uint8Array.from([100, 98, 108, 105, 112])), 'blip')
+  console.log('[TS] ✓ { decoder: BlockDecoder }')
+}
+
+function useBlockCodec<Codec extends number> (blockCodec: BlockCodec<Codec, string>) {
+  deepStrictEqual(blockCodec.code, 0x71)
+  deepStrictEqual(blockCodec.name, 'dag-cbor')
+  deepStrictEqual(Array.from(blockCodec.encode('blip')), [100, 98, 108, 105, 112])
+  deepStrictEqual(blockCodec.decode(Uint8Array.from([100, 98, 108, 105, 112])), 'blip')
+  console.log('[TS] ✓ {}:BlockCodec')
+}
+
+main()
+
+export default main
